f230950b2ce512d22edb07024a8d06d890a82430
Raz4core <mail@test.de>
2022-01-17 12:43:50 +0100

    Nachbesprechung

--- challenge_01.js
+++ challenge_01.js
@@ -10,8 +10,36 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 
- let ZahlOne = prompt("Bitte zahl eingeben");
- let ZahlTwo = prompt("Bitte Zahl eingeben");
- let summe = parseInt(ZahlOne) + parseInt(ZahlTwo);
+// let ZahlOne = prompt("Bitte zahl eingeben");
+ //let ZahlTwo = prompt("Bitte Zahl eingeben");
+ //let summe = parseInt(ZahlOne) + parseInt(ZahlTwo);
 
- console.log ("Ergebnis der Summe:" + summe);
+ //console.log ("Ergebnis der Summe:" + summe);
+
+ // Nachbesprechung
+
+//Deklaration
+let ziffer1, ziffer2;
+let zahl1, zahl2;
+let summe;
+
+// Eingabe Wertzuweisung :: Ziffern
+ziffer1 = prompt("zahl1:");
+ziffer2 = prompt("Zahl2:");
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+//Typkonvertierung / Ziffer --> Zahl (string--> number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2)
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+
+//Berechnung
+summe = zahl1 + zahl2;
+
+
+//Ausgabe
+console.log("Die Summe ist:" + summe);
+
